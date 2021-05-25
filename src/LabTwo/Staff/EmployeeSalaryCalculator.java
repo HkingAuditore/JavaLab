9370e547c66df5b86b39bcbad9458c3523d0774e
HkingAuditore <178854663@qq.com>
2021-05-25 11:29:18 +0800

    文档

--- src/LabTwo/Staff/EmployeeSalaryCalculator.java
+++ src/LabTwo/Staff/EmployeeSalaryCalculator.java
@@ -7,13 +7,14 @@ import LabTwo.Staff.Sales.SalesManager;
 import java.util.Scanner;
 
 public class EmployeeSalaryCalculator {
-	private  static final Scanner  scanner = new Scanner(System.in);
-	private  static       Employee employee;
+	private static final Scanner scanner = new Scanner(System.in);
+	private static Employee employee;
 
 	private static void generateEmployee() throws Exception {
 		System.out.println("请输入职位:");
 		String employeeTypeStr = scanner.nextLine();
-		switch (employeeTypeStr){
+		System.out.println(employeeTypeStr);
+		switch (employeeTypeStr) {
 			case "销售经理":
 				employee = new SalesManager();
 				break;
@@ -23,44 +24,44 @@ public class EmployeeSalaryCalculator {
 			case "兼职销售员":
 				employee = new PartTimeSalesman();
 				break;
-			default :
+			default:
 				throw new Exception("未输入正确的职位");
 		}
 	}
 
-	private static double inputDouble(String hint){
+	private static double inputDouble(String hint) {
 		System.out.println(hint + ":");
 		return scanner.nextDouble();
 	}
 
-	private  static void showDouble(String hint,double num){
+	private static void showDouble(String hint, double num) {
 		System.out.println(hint + "为:" + num);
 	}
 
 	public static void main(String[] args) {
 		try {
 			generateEmployee();
-			switch (employee.getEmployeeType()){
+			switch (employee.getEmployeeType()) {
 				case SALES_FULL_TIME_SALESMAN -> {
-					FullTimeSalesman fullTimeSalesman = (FullTimeSalesman)employee;
+					FullTimeSalesman fullTimeSalesman = (FullTimeSalesman) employee;
 					fullTimeSalesman.setSales(inputDouble("请输入当月销售额"));
-					showDouble("月薪",fullTimeSalesman.getMonthlySalary());
+					showDouble("月薪", fullTimeSalesman.getMonthlySalary());
 				}
 				case SALES_SALES_MANAGER -> {
-					SalesManager salesmanManager = (SalesManager)employee;
+					SalesManager salesmanManager = (SalesManager) employee;
 					salesmanManager.setSales(inputDouble("请输入当月销售总额"));
-					showDouble("月薪",salesmanManager.getMonthlySalary());
+					showDouble("月薪", salesmanManager.getMonthlySalary());
 
 				}
 				case SALES_PART_TIME_SALESMAN -> {
-					var partTimeSalesman = (PartTimeSalesman)employee;
+					var partTimeSalesman = (PartTimeSalesman) employee;
 					partTimeSalesman.setDailyWorkingHours(inputDouble("请输入工作小时数"));
-					showDouble("日薪",partTimeSalesman.getDailySalary());
+					showDouble("日薪", partTimeSalesman.getDailySalary());
 
 				}
 			}
 
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return;
 		}
 
